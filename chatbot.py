a8c80b40681293ca87faa40c61997af0c4af0b1d
Prem Saini <131263075+Learncoad@users.noreply.github.com>
2026-05-05 21:42:26 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,43 @@
+# Basic Chatbot
+
+def Response(userInput):
+    userInput = userInput.lower().strip()
+
+    #greeting
+    if userInput in ["hello",'hi','hey']:
+        return "Hi!!"
+    
+    #asking about bot
+    elif userInput in ["how are you", "how are you doing"]:
+        return "I'm fine, thanks!"
+    
+    #name
+    elif "your name" in userInput:
+        return "I am a simple chatbot."
+    
+    #help
+    elif "help" in userInput:
+        return "You can say hello, ask how I am, or type bye to exit."
+    
+    #exit
+    elif userInput in ["bye", "exit", "quit"]:
+        return "Goodbye!"
+    
+    #fallback
+    else:
+        return "Soryy, I didn't understand that."
+    
+def Chatbot():
+    print("Chatbot: Hi! Type something (type 'bye' to 'exit')")
+
+    while True:
+        userinput = input("You: ")
+
+        response = Response(userinput)
+        print("Chatbot: ", response)
+
+        if userinput.lower() in ['bye','exit','quit']:
+            break
+
+if __name__ == '__main__':
+    Chatbot()
